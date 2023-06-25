b5e3eaa6e813c356f4a61753e4898a9dc5b9b599
minhkhoiine <122579875+minhkhoiine@users.noreply.github.com>
2023-06-25 09:59:06 -0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('file').addEventListener('change', function(e) {
-    var fileName = e.target.files[0].name;
-    document.getElementById('file-name').textContent = fileName;
-  });
-  
